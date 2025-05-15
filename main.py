b5ec90ecd87b8991e3098fd0beaa92b6948f34f1
Vinicius F <barakavini@gmail.com>
2025-05-15 15:35:18 -0300

    Adicionado páginas de cadastro e login, adicionado conectividade com banco MySQL

--- main.py
+++ main.py
@@ -1,27 +1,80 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from db import db
-from models import Usuario
+from models import Funcionario
+from datetime import datetime
+import hashlib
 
 #Criar uma instância do Flask
 app = Flask(__name__)
+app.secret_key = 'A3deErivelton'
+lm = LoginManager(app)
+lm.login_view = 'login'
 
 #Adicionar database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:125867Ju@localhost/biblioteca_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Recomendado
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+def hash(txt):
+    hash_obj = hashlib.sha256(txt.encode('utf-8'))
+    return hash_obj.hexdigest()
+
+@lm.user_loader
+def user_loader(id):
+    usuario = db.session.query(Funcionario).filter_by(id=id).first()
+    return usuario
 
 #Criar uma rota pro decorator
 @app.route('/')
+@login_required
 def home():
-    usuarios = db.session.query(Usuario).filter_by(id=1).first()
-    return render_template("index.html", usuarios=usuarios)
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form.get('emailForm')
+        senha = request.form.get('senhaForm')
+
+        user = db.session.query(Funcionario).filter_by(email=email, senha=hash(senha)).first()
+        if not user:
+            return 'Nome ou senha incorretos.'
+        
+        login_user(user)
+        return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/estoque')
 def estoque():
     return render_template("estoque.html")
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'GET':
+        return render_template("cadastro.html")
+    elif request.method == 'POST':
+        nome = request.form.get('nomeForm')
+        email = request.form.get('emailForm')
+        senha = request.form.get('senhaForm')
+        senhaRepetir = request.form.get('repetirSenhaForm')
+        checkBoxTermos = request.form.get('checkBoxTermosForm')
+
+        nova_pessoa = Funcionario(nome=nome, email=email, senha=hash(senha))
+        db.session.add(nova_pessoa)
+        db.session.commit()
+        
+        login_user(nova_pessoa)
+
+        return redirect(url_for("home"))
 
 #Criar páginas de erro
 
@@ -34,3 +87,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,13 +1,32 @@
 from db import db
+from flask_login import UserMixin
 from datetime import datetime
 
-class Usuario(db.Model):
-    __tablename__ = 'Usuarios'
+class Funcionario(UserMixin, db.Model):
+    __tablename__ = 'funcionarios'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    cargo = db.Column(db.String(50), nullable=False)
-
+    cpf = db.Column(db.String(15), nullable=True, unique=True)
+    email = db.Column(db.String(100), nullable=False)
+    cargo = db.Column(db.String(20), nullable=True)
+    senha = db.Column(db.String(200), nullable=False)
+    dataContratacao = db.Column(db.DateTime, default=datetime.utcnow)
+    fotoPerfil = db.Column(db.String(200), nullable=True)
+    
     def __repr__(self):
         return f"<{self.nome}>"
+    
+class Livro(db.Model):
+    __tablename__ = 'livros'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(50), nullable=False)
+    autor = db.Column(db.String(30), nullable=False)
+    editora = db.Column(db.String(30), nullable=False)
+    anoPublicacao = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    imgCapa = db.Column(db.String(100), nullable=True)
+    nota = db.Column(db.Float, nullable=True)
+    qtdEstoque = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<{self.titulo}>"
